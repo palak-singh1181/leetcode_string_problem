d0732f1ce5982e2bd1cce99426905c0cf277e120
Palak Singh <palaksingh9503@gmail.com>
2026-06-16 07:38:12 -0700

    Add LC#81 - Search in Rotated Sorted Array II

---
+++ rotate.java
@@ -0,0 +1,39 @@
+class rotate {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) return true;
+
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            }
+            else if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        rotate sol = new rotate();
+
+        System.out.println(sol.search(new int[]{2,5,6,0,0,1,2}, 0)); // true
+        System.out.println(sol.search(new int[]{2,5,6,0,0,1,2}, 3)); // false
+    }
+}
