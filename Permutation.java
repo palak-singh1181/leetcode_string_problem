3951afb4053f89890b745da7632d8bf82e43ea81
Palak Singh <palaksingh9503@gmail.com>
2026-06-17 10:44:25 -0700

    Added Next Permutation solution

---
+++ Permutation.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class Permutation {
+
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
+
+    public static void main(String[] args) {
+        Permutation obj = new Permutation();
+
+        int[] nums = {1, 2, 3};
+
+        obj.nextPermutation(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
