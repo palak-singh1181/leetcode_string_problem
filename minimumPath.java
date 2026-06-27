027d4b65a6a030b41f77a360023f907c17cf7d44
Palak Singh <palaksingh9503@gmail.com>
2026-06-27 09:38:38 -0700

    Add LC#64 - Minimum Path Sum

---
+++ minimumPath.java
@@ -0,0 +1,24 @@
+class minimumPath {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+
+        for (int j = 1; j < n; j++)
+            grid[0][j] += grid[0][j - 1];
+
+        for (int i = 1; i < m; i++)
+            grid[i][0] += grid[i - 1][0];
+
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+
+        return grid[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        minimumPath sol = new minimumPath();
+
+        System.out.println(sol.minPathSum(new int[][]{{1,3,1},{1,5,1},{4,2,1}})); // 7
+        System.out.println(sol.minPathSum(new int[][]{{1,2,3},{4,5,6}}));         // 12
+    }
+}
