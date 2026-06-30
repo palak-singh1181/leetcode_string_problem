2cfb326af1515253c1086e4447c675b9c9cb2c60
Palak Singh <palaksingh9503@gmail.com>
2026-06-30 03:02:02 -0700

    Added Two Sum II solution

---
+++ TwoSumII.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class TwoSumII {
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        TwoSumII obj = new TwoSumII();
+
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+
+        System.out.println(Arrays.toString(obj.twoSum(numbers, target)));
+    }
+}
