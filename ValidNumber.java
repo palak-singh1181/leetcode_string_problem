cad16149acd84049f5c82744b08e1ed79959afb0
Palak Singh <palaksingh9503@gmail.com>
2026-07-01 09:01:41 -0700

    Added Valid Number solution

---
+++ ValidNumber.java
@@ -0,0 +1,49 @@
+public class ValidNumber {
+
+    public boolean isNumber(String s) {
+        s = s.trim();
+
+        boolean seenDigit = false;
+        boolean seenDot = false;
+        boolean seenExponent = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                seenDigit = true;
+            } 
+            else if (ch == '+' || ch == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            } 
+            else if (ch == '.') {
+                if (seenDot || seenExponent) {
+                    return false;
+                }
+                seenDot = true;
+            } 
+            else if (ch == 'e' || ch == 'E') {
+                if (seenExponent || !seenDigit) {
+                    return false;
+                }
+                seenExponent = true;
+                seenDigit = false;
+            } 
+            else {
+                return false;
+            }
+        }
+
+        return seenDigit;
+    }
+
+    public static void main(String[] args) {
+        ValidNumber obj = new ValidNumber();
+
+        System.out.println(obj.isNumber("0"));
+        System.out.println(obj.isNumber("e"));
+        System.out.println(obj.isNumber("."));
+    }
+}
